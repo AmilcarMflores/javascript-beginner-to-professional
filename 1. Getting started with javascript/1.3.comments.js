9b4b4ef086d526016fd208a5c53c556fc9fb37f1
Amilcar M. Flores <amilcarmflores@gmail.com>
2024-12-26 19:27:39 -0400

    26/12/2024

---
+++ 1. Getting started with javascript/1.3.comments.js
@@ -0,0 +1,6 @@
+// I'm a single line comment
+
+/*
+  I'm a multi-line comment.
+  Whatever is between the slash asterisk and the asterisk slash will not get executed.
+*/
---
+++ 1. Getting started with javascript/1.4.random_numbers.js
@@ -0,0 +1,7 @@
+console.log(Math.random());
+
+// Si queremos un número de 0 y 100:
+console.log(Math.random() * 100);
+
+// Si no queremos tener un resultado con decimal usamos Math.floor:
+console.log(Math.floor(Math.random() * 100));
